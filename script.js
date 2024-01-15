b7d9a490c81dc27ca577bffcc450b8facd124376
idreesalmasri <idrees.massri95@gmail.com>
2024-01-15 00:08:12 +0000

    finished

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-console.log('Add your name in {{name}}');
-console.log('Add the current date in {{date}}');
-console.log('Add a dialog that triggers when the visitor clicks Contact information');
+document.addEventListener("DOMContentLoaded", function () {
+    var nameElement = document.getElementById("name");
+    if (nameElement) {
+        nameElement.textContent = "Idrees Al-Masri";
+    }
+
+    var dateElement = document.getElementById("date");
+    if (dateElement) {
+        var currentDate = new Date();
+        var options = {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            timeZone: "Asia/Amman",
+        };
+        dateElement.textContent = currentDate.toLocaleDateString(
+            options
+        );
+    }
+});
+
+let popup=document.getElementById('popup');
+
+const openPopup=()=>{
+    popup.classList.add('open-popup');
+}
+const closePopup=()=>{
+    popup.classList.remove("open-popup");
+}
